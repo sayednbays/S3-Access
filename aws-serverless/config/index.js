eaaea3db9e79148dc002f0e081e08bfa73203a3a
sayednbays <zoray2ksayed@gmail.com>
2017-12-23 01:33:41 +0800

    comment

--- aws-serverless/config/index.js
+++ aws-serverless/config/index.js
@@ -1,3 +1,5 @@
+//bucketName: s3 bucket name
+// region: s3 region
 exports.S3 = {
     bucketName: 's3access-bigdata',
     region:'us-east-2'
--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,18 +1,18 @@
 export const isTest=false;
 
-// export const Cognito={
-//     region: 'us-east-2',
-//     IdentityPoolId: 'us-east-2_pA0nTIDao',
-//     UserPoolId: 'us-east-2_pA0nTIDao',
-//     ClientId: '2reeqde6d58l9lljh2mc1essi2',
-// }
-
+//region:the user pool/s3 region
+//IdentityPoolId: identity pool id
+// UserPoolId: user pool id
+// ClientId: app client id (under the user pool)
 export const Cognito={
     region: 'us-east-2',
     IdentityPoolId: 'us-east-2:dc4a6ebf-14a7-4006-a359-db87748b7645',
     UserPoolId: 'us-east-2_pA0nTIDao',
     ClientId: '3mnffd5sfb9o60e0qhk4162dl0',
 }
+
+//bucketName: s3 bucket name
+//apiEndPoint: api gateway endpoint url
 export const S3={
     bucketName:'cloud-image-20170101',
     apiEndPoint:'https://qb2g376w1d.execute-api.us-east-2.amazonaws.com/test/s3'
